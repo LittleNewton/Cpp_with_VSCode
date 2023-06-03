c13a6624cc1f0db2800eb33252f7cbd24877f3b9
Peng_Liu <littlenewton6@gmail.com>
2023-06-04 00:29:43 +0800

    [+] Add CMakeLists.txt and a related sample.

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,17 @@
 #include <array>
 #include <cstdio>
-// #include <fmt/core.h>
-
+#include <fmt/core.h>
+#include <fmt/format.h>
 
 int main() {
-    auto arr = std::to_array({1, 2, 3});
+  auto arr = std::to_array<int>({1, 2, 3, 4, 5});
 
-    printf("Hello, world!\n");
-    return 0;
+  for (int val : arr) {
+    fmt::print("{0}", val);
+  }
+
+  printf("\n");
+
+  printf("Hello, world!\n");
+  return 0;
 }
