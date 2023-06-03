93b2d0174ebdd2a1e1da84935862f6a9e09618a2
Peng_Liu <littlenewton6@gmail.com>
2023-06-03 23:52:08 +0800

    [+] Add sample project

---
+++ Makefile
@@ -0,0 +1,2 @@
+hello: hello.cpp
+	clang++ --std=c++20 -o build/hello hello.cpp
---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <array>
+#include <cstdio>
+// #include <fmt/core.h>
+
+
+int main() {
+    auto arr = std::to_array({1, 2, 3});
+
+    printf("Hello, world!\n");
+    return 0;
+}
